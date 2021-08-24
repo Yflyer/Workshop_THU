c14c5e68b3a30b83a98a69ff2f5eba95a5bf7f4a
yflyer <zengyf93@qq.com>
2021-08-24 14:29:39 +0800

    refresh R script of cluster

--- shotgun_tutorial/cluster_example/cluster_table.R
+++ shotgun_tutorial/cluster_example/cluster_table.R
@@ -35,7 +35,7 @@ colnames(cluster_info)=c('rep_seq','mem_seq')
 # mem_seq = cluster_info %>% distinct(mem_seq) 
 # rep_seq = cluster_info %>% distinct(rep_seq) 
 
-sample_list = fs::dir_ls(output,type='file')
+sample_list = fs::dir_ls(input,type='file')
 sample_name = strsplit(sample_list,'/') %>% rapply(., function(x) (tail(x,1))) # remove base dir
 sample_name = strsplit(sample_name,'\\.') %>% rapply(., function(x) (head(x,1))) # remove tail
 
